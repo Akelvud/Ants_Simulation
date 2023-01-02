264d7ca6ec494d24a2206612ce26e727360add51
Akelvud <akulenko.slava@gmail.com>
2023-01-02 17:37:32 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+int SCREEN_WIDTH = 900;
+int SCREEN_HEIGHT = 600;
+
+SDL_Window *win = NULL;
+SDL_Renderer *ren = NULL;
+
+bool init() {
+    bool ok = true;
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        cout << "Can't init SDL: " << SDL_GetError() << endl;
+    }
+
+    win = SDL_CreateWindow("Ants Simulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (win == NULL) {
+        cout << "Can't create window: " << SDL_GetError() << endl;
+        ok = false;
+    }
+
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+    if (ren == NULL) {
+        cout << "Can't create renderer: " << SDL_GetError() << endl;
+        ok = false;
+    }
+    return ok;
+}
+
+void quit() {
+    SDL_DestroyWindow(win);
+    win = NULL;
+
+    SDL_DestroyRenderer(ren);
+    ren = NULL;
+
+    SDL_Quit();
+}
+
+
+int main(int32_t arhc, char ** argv) {
+
+    init();
+
+    quit();
+    return 0;
+}
